c100605ed91c600332a098850ad956261a754259
Asetiawan <onezcube@gmail.com>
2018-05-15 22:45:59 +0700

    Create Role Model_1

--- app/Role.php
+++ app/Role.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
-    public function role(){
-        
-            return $this->belongsTo('App\Role');
-        
-        
-        }
+    // 
+    
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // Koneksi ke data Migration
+    public function role(){
+        
+            return $this->belongsTo('App\Role');
+        
+        
+        }
 }
